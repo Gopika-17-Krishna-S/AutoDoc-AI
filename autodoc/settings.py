e83a5f2e3854bfdc54ad5e7d302489b876211fda
Gopika-17-Krishna-S <gopikakrishnas173@gmail.com>
2026-04-10 11:00:54 +0530

    Add custom logo and configure static files

--- autodoc/settings.py
+++ autodoc/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Media files (uploaded ZIPs, etc.)
 MEDIA_URL = '/media/'
